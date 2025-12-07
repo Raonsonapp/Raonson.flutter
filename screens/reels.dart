451b58eca9319cfda8e0cd45c9374b8b865c357e
Raonson <ehsonmahmadmurodov@gmail.com>
2025-12-07 11:57:30 +0500

    Create reels.dart

---
+++ screens/reels.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ReelsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Center(
+        child: Text(
+          "Reels Section",
+          style: TextStyle(color: Colors.white, fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
